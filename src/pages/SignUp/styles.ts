4c88ecfb9609274644c6bcd8dacc440f9104b24a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-10-25 19:57:00 -0300

    create register page

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/3.jpeg';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 70px;
+      color: #1a1e2c;
+    }
+
+    a {
+      font-size: 20px;
+      color: #a18de7;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#A18DE7')};
+      }
+    }
+  }
+
+  > a {
+    font-size: 20px;
+    color: #a18de7;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#A18DE7')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
