4c03f5006a48e58ae52c339a37cfed4781af69d2
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-10-26 08:28:35 -0500

    add effects in input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,11 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #d5cdf1;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
   color: #1a1e2c;
 
   display: flex;
@@ -14,6 +20,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #612f74;
+      border-color: #612f74;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #612f74;
+    `}
 
   input {
     flex: 1;
