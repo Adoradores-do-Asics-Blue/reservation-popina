ba43ae3fbdec649f4b376591f866b34ecf1e784c
RicardoEstudante <ricardo2010carvalho@hotmail.com>
2020-10-27 08:43:54 -0500

    Refactoring useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,34 +1,64 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
-interface AuthContextState {
-  name: string;
+interface AuthContextData {
+  user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextState>(
-  {} as AuthContextState,
-);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Popina:token');
+    const user = localStorage.getItem('@Popina:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    // eslint-disable-next-line no-console
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@Popina:token', token);
+    localStorage.setItem('@Popina:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Ricardo', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('UseAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
